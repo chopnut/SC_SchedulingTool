768db3d0e89d173f1edfde3c619072ca5c233d5f
Ernani.Danting <ernani.danting@smartcomm.net.au>
2017-09-05 16:06:09 +1000

    Miscellaneous edits

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,6 @@
 <?php 
 
- 
+ $app->get('/', function(){
+
+ 		echo 'Hello world';
+ });
--- start.php
+++ start.php
@@ -10,13 +10,13 @@ session_start();
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim([
-	'view'=> new \Slim]Views\Twig()
+	'view'=> new \Slim\Views\Twig()
 ]);
 
 $view = $app->view();
 $view->setTemplatesDirectory('app/views');
 $view->parserExtensions = [
-	new \Slim\Views\TwigExtension()
+	new \Slim\Views\TwigExtension(),
 ];
 
 require ('app/routes.php');
