b554b5c57f1181c47885d517d3f8af7006ab9f79
Ernani.Danting <ernani.danting@smartcomm.net.au>
2017-09-06 12:43:28 +1000

    Miscellaneous edits

--- app/views/index.php
+++ app/views/index.php
@@ -10,8 +10,12 @@
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css">
+        <link rel="stylesheet" href="../bower_components/semantic/dist/semantic.min.css">
+        <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css">
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+        <link rel="stylesheet" href="css/app.css">
+        <script src="js/app.js" type="text/javascript"></script>
         <!--<link rel="stylesheet" href="css/main.css"> -->
     </head>
     <body>
@@ -19,17 +23,41 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 {{ v }}</p>
+        <!-- This will house the login/logout content -->
+        <div class="wrapper-header">
+            <div class="container-header">
+                <div class="left">
+                    <div class="logo">
+                        <img src="assets/logo-large-09.png" alt="Smartcomm">
+                    </div>
+                    <div class="logo_left">
+                        STUFF HERE
+                    </div>
+                </div>
+                <div class="right">
+                    LEFTSTUFF HERE
+                </div>
+        
+            </div>  <!-- END wrapper-container-header -->
+        </div> <!-- END wrapper-header-->
 
-        <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
+        <!-- this will house the option for the report -->
+        <div class="wrapper-header-option">
+            <div class="container-header-option"></div>
+        </div>    
+        <!-- this will house the main view of the content -->
+        <div class="wrapper-view-content">
+            <div class="container-view-content"></div>
+        </div>
+        <script href="../bower_components/jquery/dist/jquery.min.js"></script>
+        <script href="../bower_components/semantic/dist/semantic.min.js"></script>
+
+       
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
             window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
             ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
         </script>
-        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
     </body>
 </html>
