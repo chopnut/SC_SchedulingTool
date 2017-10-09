39be9c37593e626553eac946b009a568dca37d81
Ernani Danting <bprizze@yahoo.com>
2017-10-10 04:57:51 +1000

    Miscellaneous edits

--- entry.js
+++ entry.js
@@ -3,14 +3,12 @@ require("./node_modules/semantic-ui/dist/semantic.css");
 require("./node_modules/jquery-ui-dist/jquery-ui.css");
 require("./node_modules/jquery-ui-dist/jquery-ui.theme.css");
 require("./node_modules/semantic-ui/dist/components/tab.css");
-
-
 require("expose-loader?$!expose-loader?jQuery!./node_modules/jquery/dist/jquery.js");
+
 require("./node_modules/jquery-ui-dist/jquery-ui.js");
 require("./node_modules/semantic-ui/dist/semantic.js");
 require("./node_modules/semantic-ui/dist/components/accordion.js");
 require("./node_modules/semantic-ui/dist/components/tab.js");
-
 
 
 import React from 'react';
