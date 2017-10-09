e64e25edea77cdaadf3bf5eb6c2127c733649c76
Ernani Danting <bprizze@yahoo.com>
2017-10-10 06:50:04 +1000

    Miscellaneous edits

--- entry.js
+++ entry.js
@@ -5,6 +5,7 @@ require("./node_modules/jquery-ui-dist/jquery-ui.theme.css");
 require("./node_modules/semantic-ui/dist/components/tab.css");
 require("expose-loader?$!expose-loader?jQuery!./node_modules/jquery/dist/jquery.js");
 
+require("./public/assets/css/app.scss");
 require("./node_modules/jquery-ui-dist/jquery-ui.js");
 require("./node_modules/semantic-ui/dist/semantic.js");
 require("./node_modules/semantic-ui/dist/components/accordion.js");
