6943c6a3c12c662b64512cdcdcdfcf1a12e8cc2e
Ernani Danting <bprizze@yahoo.com>
2017-10-16 09:36:24 +1000

    Miscellaneous edits

---
+++ react/reducers/reducers.js
@@ -0,0 +1,5 @@
+import Redux from 'redux';
+const tabReducer = function (state, action) {
+
+}
+export default tabReducer;
---
+++ react/store/store.js
@@ -0,0 +1,19 @@
+import {createStore} from 'redux';
+import TabReducer from '../reducers/reducers';
+
+
+const initialState =window.__initial_state__;
+const MainStore = createStore(function (state, action) {
+    switch (action.type){
+        case 'SAMPLE':
+            let myObj = Object.assign({},action.data,{
+                sample: "Hello World"
+            })
+            return Object.assign({},state,myObj);
+        default:
+            return state || {} // If there is no state yet just return an empty object
+    }
+},initialState);
+
+
+export default MainStore;
