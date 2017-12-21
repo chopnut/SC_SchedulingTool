a91dd62a40ddac458215e4e9a2e7f2162855e8a4
Ernani.Danting <ernani.danting@smartcomm.net.au>
2017-12-21 16:30:13 +1000

    Miscellaneous edits

--- references/for_schedule_tool_only/sc-webapps.sql
+++ references/for_schedule_tool_only/sc-webapps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2017 at 06:49 AM
+-- Generation Time: Dec 21, 2017 at 06:29 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
@@ -695,9 +695,16 @@ INSERT INTO `sched_settings` (`id`, `setting_name`, `setting_label`, `setting_va
 CREATE TABLE `sched_user_settings` (
   `sched_us_id` int(11) NOT NULL,
   `login_id` int(11) NOT NULL,
-  `sched_us_calendar_hide_departments` varchar(100) NOT NULL,
-  `sched_us_department_group` varchar(100) NOT NULL
+  `sched_us_calendar_hide_departments` varchar(100) DEFAULT NULL,
+  `sched_us_department_group` varchar(100) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sched_user_settings`
+--
+
+INSERT INTO `sched_user_settings` (`sched_us_id`, `login_id`, `sched_us_calendar_hide_departments`, `sched_us_department_group`) VALUES
+(16, 1, '[]', '[]');
 
 --
 -- Indexes for dumped tables
@@ -840,7 +847,7 @@ ALTER TABLE `sched_settings`
 -- AUTO_INCREMENT for table `sched_user_settings`
 --
 ALTER TABLE `sched_user_settings`
-  MODIFY `sched_us_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sched_us_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
