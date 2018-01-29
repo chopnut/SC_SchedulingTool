445153f7d392a5c1fe5ca572e1b2c3db2948fe08
Ernani.Danting <ernani.danting@smartcomm.net.au>
2018-01-29 17:00:46 +1000

    Miscellaneous edits

--- references/for_schedule_tool_only/sc-webapps.sql
+++ references/for_schedule_tool_only/sc-webapps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 29, 2018 at 03:39 AM
+-- Generation Time: Jan 29, 2018 at 07:00 AM
 -- Server version: 5.7.14
 -- PHP Version: 5.6.25
 
