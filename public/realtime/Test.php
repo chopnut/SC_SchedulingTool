ed3688b9233e590390f852e392f3e079f15014b5
Ernani.Danting <ernani.danting@smartcomm.net.au>
2018-04-06 16:54:20 +1000

    Miscellaneous edits

--- public/realtime/Test.php
+++ public/realtime/Test.php
@@ -9,9 +9,15 @@ $entryData = array(
 
 $context    = new \ZMQContext();
 $socket     = $context->getSocket(ZMQ::SOCKET_PUSH,'SCSTRealtimeSubsObject');
-$socket->connect('tcp://localhost:11111');
-sleep(2);
-$socket->send(json_encode($entryData));
+$socket->setSockOpt(ZMQ::SOCKOPT_LINGER, 500);
+$socket->connect('tcp://192.168.70.9:11111');
+
+$i = 0;
+while($i < 10){
+    $i++;
+    echo "SENDING ...\n";
+    $socket->send("HELLO WORLD");
+}
 
 echo "SocketType: ".$socket->getSocketType();
 print_r($socket->getEndpoints());
